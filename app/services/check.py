134a3b031cf787c69e48f400d488b4db0b6d52b6
Cheaple <chenyulechn@gmail.com>
2022-11-29 20:11:06 +0800

    modify check error message

--- app/services/check.py
+++ app/services/check.py
@@ -42,7 +42,7 @@ class CheckService():
             count = [dict(zip(result.keys(), result))
                      for result in count_result]
 
-            return check_list, count[0]['count'],"ok", True
+            return check_list, count[0]['count'],"ok get check list", True
 
         except Exception as e:
             print(e)
@@ -60,7 +60,7 @@ class CheckService():
             ).filter(Check.id == id).first()
             if result is None:
                 return None, "check not found", False
-            return dict(zip(result.keys(), result)), True
+            return dict(zip(result.keys(), result)), "ok get check", True
         except Exception as e:
             print(e)
             return None, "error", False
@@ -77,7 +77,7 @@ class CheckService():
             )
             db.session.add(check)
             db.session.commit()
-            return check.id, "ok", True
+            return check.id, "ok add check", True
         except Exception as e:
             print(e)
             return 0, "error", False
@@ -85,12 +85,14 @@ class CheckService():
     def update_check(self, id, content):
         try:
             check = Check.query.get(id)
+            if check is None:
+                return 0, "check not found", False
 
             if 'info' in content:
                 check.info = content['info']
 
             db.session.commit()
-            return check.id, "ok", True
+            return check.id, "ok update check", True
         except Exception as e:
             print(e)
             return 0, "error", False
